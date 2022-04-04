94a24c5c17f76d177ec86dcec6305dd36277b601
danalek <101091162+danalek@users.noreply.github.com>
2022-04-04 10:07:10 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+#Pirmā lapa, kas tiks ielādēta
+@app.route('/',methods = ['POST', 'GET'])
+def root():
+    return render_template("index.html")
+    
+#Pārbaudes lapa, lai saprastu, ka kods vispār strādā
+@app.route('/about')
+def about():
+  return render_template("about.html")
+
+if __name__ == '__main__':
+  app.run(debug="true")
