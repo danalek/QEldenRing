acf315db0e3631171da57f0a46df04c9a9e0bd7a
danalek <101091162+danalek@users.noreply.github.com>
2022-04-04 10:43:56 +0300

    sdf
    wwerfrw

--- server.py
+++ server.py
@@ -4,15 +4,35 @@ from flask import render_template
 
 app = Flask(__name__)
 
+def loadAllPictures():
+  filepath = 'picDB.txt'
+  pictures=[]
+  file = open(filepath, "r")
+  text = file.read()
+  images = text.strip().split("\n")
+  file.close()
+  print(images)
+  return images
+
 #Pirmā lapa, kas tiks ielādēta
 @app.route('/',methods = ['POST', 'GET'])
 def root():
     return render_template("index.html")
-    
-#Pārbaudes lapa, lai saprastu, ka kods vispār strādā
+
 @app.route('/about')
 def about():
   return render_template("about.html")
+
+@app.route('/test',methods = ['POST', 'GET'])
+def test():
+    parametri = ["IQ","Augums","Kāja"]
+    images = loadAllPictures()
+    return render_template("test.html",parametri=parametri,images=images)
+
+#Pārbaudes lapa, lai saprastu, ka kods vispār strādā
+@app.route('/health')
+def health():
+  return "Viss kārtībā!"
 
 if __name__ == '__main__':
   app.run(debug="true")
